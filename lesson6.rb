89d853684e4fe2d3d00a4ea499d0dbb5a7e8423d
kazutotaniyoshi <it352032@gmail.com>
2021-10-02 13:32:14 +0000

    test

---
+++ lesson6.rb
@@ -0,0 +1,8 @@
+total_price = 100
+if total_price >100
+  puts "みかんを購入し、所持金に余りあり"
+elsif total_price == 100
+  puts "みかんを購入して、所持金は０"
+elsif total_price < 100
+  puts "みかんを購入することができません"
+end
---
+++ lesson7-2.rb
@@ -0,0 +1,12 @@
+puts "計算を始めます"
+puts "何回計算をしますか？"
+input = gets,to_i
+puts i  
+puts "２つの数字を入力してください"
+a =gets.to_i
+b =gets.to_i
+puts "計算結果を出力します"
+puts "a*b = #{a*b}"
+puts "#{gets,to_i}回目の計算です"
+puts "計算を終了します"
+end
