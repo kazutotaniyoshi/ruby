ea81085be2aeff3484b9a18cd2095483d18a5222
kazutotaniyoshi <it352032@gmail.com>
2021-10-02 04:29:56 +0000

    test

---
+++ else.rb
@@ -0,0 +1,9 @@
+apple = "Nagano"
+if apple == "Aomori"
+puts "このリンゴは青森産です"
+elsif apple == "Nagano"
+puts "このリンゴは長野産ではなく青森産です"
+eles
+puts "このリンゴは青森産でも長野産でもありません"
+end
+  
--- score.rb
+++ score.rb
@@ -2,3 +2,7 @@ score = 70
 if (score <= 50 || score <= 100) && score >= 80
 puts "スコアは50点以上、100点以下　かつ80点以下です。" 
 end
+
+if score >=50 || ( score >=100 && score >=80 )
+puts "スコアは50点以上または80点以上100点以下"
+end
